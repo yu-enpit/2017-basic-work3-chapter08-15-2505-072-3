0003ea97d7e5d4497436b25a30c75bee2c0103dc
enPiT-P13 <v072ff@yamaguchi-u.ac.jp>
2017-08-09 14:51:43 +0900

    Chapter8提出です

--- MyScheduler/app/src/main/java/com/example/yu_enpit/myscheduler/MainActivity.java
+++ MyScheduler/app/src/main/java/com/example/yu_enpit/myscheduler/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.yu_enpit.myscheduler;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
+    private Realm mRealm;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,31 +27,34 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this,ScheduleEditActivity.class));
             }
         });
+        mRealm = Realm.getDefaultInstance();
+
+        mListView = (ListView) findViewById(R.id.listView);
+        RealmResults<Schedule> schedules
+                = mRealm.where(Schedule.class).findAll();
+        ScheduleAdapter adapter = new ScheduleAdapter(schedules);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view,
+                                            int position, long id) {
+                        Schedule schedule =
+                                (Schedule) parent.getItemAtPosition(position);
+                        startActivity(new Intent(MainActivity.this,
+                                ScheduleEditActivity.class)
+                                .putExtra("schedule_id", schedule.getId()));
+                    }
+                });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onDestroy() {
+        super.onDestroy();
+        mRealm.close();
     }
 }

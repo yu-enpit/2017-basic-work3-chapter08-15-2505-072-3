74e31a962b0aa7636ff2cceefc62b252f57525b1
enPiT-P13 <v072ff@yamaguchi-u.ac.jp>
2017-09-01 11:47:56 +0900

    Chapter8再提出です

--- MyScheduler/app/src/main/java/com/example/yu_enpit/myscheduler/MainActivity.java
+++ MyScheduler/app/src/main/java/com/example/yu_enpit/myscheduler/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -56,5 +58,20 @@ public class MainActivity extends AppCompatActivity {
     public void onDestroy() {
         super.onDestroy();
         mRealm.close();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
